c14dcaab5baafd45ba6fb508fb4ca2ad472596a9
moka236 <medosd236@gmail.com>
2023-11-27 02:52:40 -0800

    py3

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,21 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+
 /**
- * check the cycle - check if list cycle ocurr
- * @list : point to the list to check
- * return 1 if cycle oucrr , 0 otherwise 
+ * check_cycle - checks if a singly linked list has
+ * a cycle in it
+ * @list: pointer to the list
+ * Return: 0 if there is no cycle,
+ * 1 if there is a cycle
  */
-int check_cycle(listint_t *list);
+int check_cycle(listint_t *list)
 {
-	listint_t *lis1 = list , lis2 = list;
-	
-	while (lis2 && lis2->next)
+	listint_t *lis1;
+	listint_t *lis2;
+
+	lis1 = list;
+	lis2 = list;
+	while (list && lis1 && lis1->next)
 	{
-		lis1 = lis1->next;
-		lis2 = lis2->next->next;
-		if(lis1==lis2)
-			 return (1);
+		list = list->next;
+		lis1 = lis1->next->next;
+
+		if (list == lis1)
+		{
+			list = lis2;
+			lis2 =  lis1;
+			while (1)
+			{
+				lis1 = lis2;
+				while (lis1->next != list && lis1->next != lis2)
+				{
+					lis1 = lis1->next;
+				}
+				if (lis1->next == list)
+					break;
+
+				list = list->next;
+			}
+			return (1);
+		}
 	}
+
 	return (0);
 }
