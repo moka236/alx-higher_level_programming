59a4fbc26bfac6d33143ae312cf5e3864f780345
moka236 <medosd236@gmail.com>
2023-11-28 02:27:15 -0800

    if

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(0,10):
-    for n in range(i,10):
-        if i < n:
-            print("{:d}{:d}".format(i,n), end="\n" if i == 9 else ", ")
+for digit1 in range(0, 9):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8:
+            print("{:d}{:d}".format(digit1, digit2))
+            break
+        print("{:d}{:d}".format(digit1, digit2), end=", ")
