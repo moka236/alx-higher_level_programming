04928cb38d60a69e1bd58a3998f22fcb368ffa50
moka236 <medosd236@gmail.com>
2023-12-19 18:49:55 -0800

    classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """ class Square that defines a square"""
 
+
 class Square:
     """ class Square that defines a square"""
-    pass
+    def __init__(self, size):
+        """initialize square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
